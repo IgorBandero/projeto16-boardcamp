13be7066b5a27fee668b41e7c8c4b7927bb755c4
Igor Bandero <igorbandero@gmail.com>
2023-07-31 12:47:53 -0300

    fix: correção da atualização do customer

--- src/middlewares/validationCustomer.js
+++ src/middlewares/validationCustomer.js
@@ -24,7 +24,7 @@ export async function checkRepeatedUpdatedCostumer(req, res, next){
 
     try {
         const customer = await db.query(`SELECT * FROM customers WHERE cpf = $1;`, [cpf]);
-        if (customer.rows.length > 1){
+        if (customer.rows.length > 0){
             res.status(409).send("Cliente já existe!");
         }
         next();
